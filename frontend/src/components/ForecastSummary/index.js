7c3739d4ff318a3ff4dfcc403ebe4f94c3d6c6c6
djhir3 <djhir3@gmail.com>
2021-12-14 15:49:12 +1100

    Text for when forecast is being loaded

--- frontend/src/components/ForecastSummary/index.js
+++ frontend/src/components/ForecastSummary/index.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import styles from './ForecastSummary.module.css';
 
-const ForecastSummary = (props) => (
-     // This makes sure the component does not display until a forecast is actually loaded
-    props.forecast.overallWinPercent !== undefined &&
-    <div className={styles.summary}>
-        {props.forecast.name}
-        <br/>
-        Last updated: {props.forecast.date}
-        <br/>
-        Updated because of: {props.forecast.description}
-        <br/>
-        ALP win percent: {props.forecast.overallWinPercent === undefined ? '' : Number((props.forecast.overallWinPercent.alp).toFixed(2))}
-        <br/>
-        LNP win percent: {props.forecast.overallWinPercent === undefined ? '' : Number((props.forecast.overallWinPercent.lnp).toFixed(2))}
-        <br/>
-        OTH win percent: {props.forecast.overallWinPercent === undefined ? '' : Number((props.forecast.overallWinPercent.oth).toFixed(2))}
-    </div>
-    
+const ForecastSummaryVisible = props => (
+    // This makes sure the component does not display until a forecast is actually loaded
+   props.forecast.overallWinPercent !== undefined &&
+   <div className={styles.summary}>
+       {props.forecast.name}
+       <br/>
+       Last updated: {props.forecast.date}
+       <br/>
+       Updated because of: {props.forecast.description}
+       <br/>
+       ALP win percent: {props.forecast.overallWinPercent === undefined ? '' : Number((props.forecast.overallWinPercent.alp)).toFixed(1)}
+       <br/>
+       LNP win percent: {props.forecast.overallWinPercent === undefined ? '' : Number((props.forecast.overallWinPercent.lnp)).toFixed(1)}
+       <br/>
+       OTH win percent: {props.forecast.overallWinPercent === undefined ? '' : Number((props.forecast.overallWinPercent.oth)).toFixed(1)}
+   </div>
 );
+
+const ForecastSummaryLoading = () => (
+   <div className={styles.summary}>
+       Loading forecast
+   </div>
+)
+
+const ForecastSummary = props => {
+     // This makes sure the component does not display until a forecast is actually loaded
+    if (props.forecast.overallWinPercent !== undefined) {
+        return <ForecastSummaryVisible forecast={props.forecast}  />
+    } else {
+        return <ForecastSummaryLoading />
+    }
+}
 
 export default ForecastSummary;
