1d4a192e03c751319d2b89cd7eda50593a412b71
djhir3 <djhir3@gmail.com>
2021-12-29 01:43:34 +1100

    Order partys in winner bar by ideology

--- frontend/src/components/Seats/index.js
+++ frontend/src/components/Seats/index.js
@@ -10,12 +10,31 @@ import { SmartBadge } from '../PartyBadge'
 import { intMap } from '../../utils/intmap.js'
 
 import styles from './Seats.module.css';
+import { standardiseParty } from 'utils/partyclass';
+
+const partyCategory = (party, forecast) => {
+    const sp = standardiseParty(party).toLowerCase();
+    if (sp === 'grn') return -2;
+    if (sp === 'alp') return -1;
+    if (sp === 'kap') return 1;
+    if (sp === 'lnp') return 2;
+    if (sp === 'onp') return 3;
+    if (sp === 'uap') return 3;
+    return 0;
+}
 
 const SeatRow = props => {
     const seatName = props.forecast.seatNames[props.index];
     const incumbentIndex = props.forecast.seatIncumbents[props.index];
     const incumbentAbbr = intMap(props.forecast.partyAbbr, incumbentIndex);
     const margin = Math.abs(props.forecast.seatMargins[props.index]);
+
+    const freqs = props.forecast.seatPartyWinFrequencies[props.index];
+    freqs.sort((a, b) => {
+        const aName = intMap(props.forecast.partyAbbr, a[0]);
+        const bName = intMap(props.forecast.partyAbbr, b[0]);
+        return partyCategory(aName) - partyCategory(bName);
+    });
 
     // const thresholds = [[0,2,0],[2,4,1],[4,6,2],[6,8,3],[8,10,4],[10,12,5],[12,14,6]];
     return (
