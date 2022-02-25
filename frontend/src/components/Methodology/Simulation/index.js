2e125b0b9818d1cd1953aefd8b8588d1b45104e9
djhir3 <djhir3@gmail.com>
2022-02-25 23:19:37 +1100

    Add methodology section for TPP simulation

--- frontend/src/components/Methodology/Simulation/index.js
+++ frontend/src/components/Methodology/Simulation/index.js
@@ -60,9 +60,40 @@ const MethodologyPollTrend = props => {
             </p>
             <h5 id="two-party-seat">Two-party seat vote</h5>
             <p>
-                Following the generation of each region's TPP swingsThe first step of the seat simulation is to generate a two-party-preferred result for each
-                seat. There are several parts to this. First the general two-party swing is applied, obtained
-                from comparing the 
+                Following the generation of each region's TPP swings, the next step of the seat simulation is to generate a
+                two-party-preferred result for each seat. (This is done even for seats where the the final two were not
+                the major parties.) The regional two-party swing is applied to the existing margin
+                in the seat (accounting for the redistributed margin according
+                to <ExtLink href="https://antonygreen.com.au/category/redistribution/">Antony Green's estimates</ExtLink>,
+                including for draft redistributions). This is then adjusted based on statistics calculated from the seat's
+                history, specifically its <i>elasticity</i> - whether it tends to swing more or less than the regional
+                average - and <i>trend</i> - its long term tendency to move towards one party or another. The analysis
+                of these variables includes previous seats covering a similar area with a different name, and if the seat
+                is new the statistics from other seats are used.
+            </p>
+            <p>
+                The TPP for each seat is also adjusted for candidate factors - such as retirements, and the
+                "sophomore surge" where newly elected candidates and parties do better than the region average
+                for their second election. (Sophomore effects for new candidates and parties are treated as
+                separate, with both added together when a candidate takes a seat from the opposing party).
+                These adjustments are determined by analysing previous elections separately for urban and
+                regional seats. An adjustment is also made for candidate disendorsements, it is assumed that
+                a candidate loses 3% TPP on average for being disendorsed and the party gains half that much (1.5%)
+                back following that election.
+            </p>
+            <p>
+                Finally, random variability is added to each seat. The standard deviation of such variability is again
+                assessed on a seat-by-seat basis as some seats tend to be more volatile than others even after
+                accounting for the above factors. However, the variability is given a minimum variability level
+                as due to the fairly small sample sizes involved it is quite likely that some seats' variability
+                is underestimated.
+            </p>
+            <p>
+                The above adjustments mean that usually the population-weighted average of seat TPPs no longer
+                matches the regional TPP calculated before. In order to keep all aspects of the simulation consistent,
+                the TPPs are adjusted linearly so that the seat average TPP for each region matches the overall TPP.
+                (Keep in mind that the regional TPPs have already been adjusted to match the election TPP, so this
+                adjustment also brings the seat TPPs in line with the election TPP.)
             </p>
             <p>
                 <ExtLink href="https://armariuminterreta.com/2021/08/05/australian-economy-electoral-impact/">this analysis 
