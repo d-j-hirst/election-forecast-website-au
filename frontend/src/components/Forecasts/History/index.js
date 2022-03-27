c817e3449266c40aa7a7fa86188797e4aaa98cc0
djhir3 <djhir3@gmail.com>
2022-03-28 00:04:52 +1100

    Proof of concept for forecast history graph.

--- frontend/src/components/Forecasts/History/index.js
+++ frontend/src/components/Forecasts/History/index.js
@@ -4,6 +4,8 @@ import Alert from 'react-bootstrap/Alert';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
+import { ComposedChart, Line, XAxis, YAxis, ZAxis, ReferenceLine, Area, Scatter, Tooltip, ResponsiveContainer } from 'recharts';
+
 import StandardErrorBoundary from '../../General/StandardErrorBoundary';
 import InfoIcon from '../../General/InfoIcon';
 import LoadingMarker from '../../General/LoadingMarker';
@@ -11,6 +13,85 @@ import LoadingMarker from '../../General/LoadingMarker';
 import { getDirect } from 'utils/sdk';
 
 import styles from './VoteTotals.module.css';
+import { jsonMap } from 'utils/jsonmap';
+
+const colours = [["ALP", ["#FF0000", "#FF4444", "#FFAAAA", "#FFCCCC"]],
+                 ["LNP", ["#0000FF", "#4444FF", "#AAAAFF", "#CCCCFF"]],
+                 ["GRN", ["#008800", "#22CC00", "#66FF44", "#BBFF99"]],
+                 ["ONP", ["#AA6600", "#FF7F00", "#FFAB58", "#FFC388"]],
+                 ["UAP", ["#886600", "#C2B615", "#EBDF43", "#F0E87C"]],
+                 ["SAB", ["#886600", "#C2B615", "#EBDF43", "#F0E87C"]],
+                 ["OTH", ["#777777", "#999999", "#C5C5C5", "#E0E0E0"]]];
+
+const tieColour = "#885588"
+
+const Chart = props => {
+    const tempDates = props.data.map(a => new Date(a.date).getTime() / 86400000);
+    const prevDate = Math.min.apply(Math, tempDates);
+    const adjDates = tempDates.map(a => a - prevDate);
+    const alpMaj = props.data.map(a => jsonMap(a.majorityWinPc, 0));
+    const alpMin = props.data.map((a, index) => jsonMap(a.minorityWinPc, 0));
+    const alpMost = props.data.map((a, index) => jsonMap(a.mostSeatsWinPc, 0));
+    const ties = props.data.map((a, index) => 100 -
+        a.majorityWinPc.reduce((a, b) => a + b[1], 0) - 
+        a.minorityWinPc.reduce((a, b) => a + b[1], 0) -
+        a.mostSeatsWinPc.reduce((a, b) => a + b[1], 0));
+    const othLeads = props.data.map((a, index) => 
+        a.majorityWinPc.reduce((a, b) => a + (b[0] > 1 || b[0] < 0 ? b[1] : 0), 0) + 
+        a.minorityWinPc.reduce((a, b) => a + (b[0] > 1 || b[0] < 0 ? b[1] : 0), 0) +
+        a.mostSeatsWinPc.reduce((a, b) => a + (b[0] > 1 || b[0] < 0 ? b[1] : 0), 0));
+    const lnpMost = props.data.map((a, index) => jsonMap(a.mostSeatsWinPc, 1));
+    const lnpMin = props.data.map((a, index) => jsonMap(a.minorityWinPc, 1));
+    const lnpMaj = props.data.map((a, index) => jsonMap(a.majorityWinPc, 1));
+
+    const alpMinStacked = alpMin.map((a, index) => a + alpMaj[index]);
+    const alpMostStacked = alpMost.map((a, index) => a + alpMinStacked[index]);
+    const tiesStacked = ties.map((a, index) => a + alpMostStacked[index]);
+    const othLeadsStacked = othLeads.map((a, index) => a + tiesStacked[index]);
+    const lnpMostStacked = lnpMost.map((a, index) => a + othLeadsStacked[index]);
+    const lnpMinStacked = lnpMin.map((a, index) => a + lnpMostStacked[index]);
+    const lnpMajStacked = lnpMaj.map((a, index) => a + lnpMinStacked[index]);
+    const trendData = adjDates.map((date, index) => ({
+        "date": date,
+        "alpMaj": [0, alpMaj[index]],
+        "alpMin": [alpMaj[index], alpMinStacked[index]],
+        "alpMost": [alpMinStacked[index], alpMostStacked[index]],
+        "ties": [alpMostStacked[index], tiesStacked[index]],
+        "othLeads": [tiesStacked[index], othLeadsStacked[index]],
+        "lnpMost": [othLeadsStacked[index], lnpMostStacked[index]],
+        "lnpMin": [lnpMostStacked[index], lnpMinStacked[index]],
+        "lnpMaj": [lnpMinStacked[index], lnpMajStacked[index]]
+    }));
+
+    return (
+        <>
+        {trendData !== undefined &&
+            <ResponsiveContainer width="100%" height={400}>
+                <ComposedChart
+                    width={730}
+                    height={250}
+                    data={trendData}
+                    margin={{
+                    top: 20, right: 20, bottom: 20, left: 20,
+                    }}
+                >
+                    <XAxis type="number" dataKey="date" domain={[0, 100]}/>
+                    <YAxis type="number" domain={[0, 100]}/>
+                    <Area dataKey="alpMaj" type="stepAfter" activeDot={false} isAnimationActive={false} fill={jsonMap(colours, "ALP")[0]} />
+                    <Area dataKey="alpMin" type="stepAfter" activeDot={false} isAnimationActive={false} fill={jsonMap(colours, "ALP")[1]} />
+                    <Area dataKey="alpMost" type="stepAfter" activeDot={false} isAnimationActive={false} fill={jsonMap(colours, "ALP")[2]} />
+                    <Area dataKey="ties" type="stepAfter" activeDot={false} isAnimationActive={false} fill={tieColour} />
+                    <Area dataKey="othLeads" type="stepAfter" activeDot={false} isAnimationActive={false} fill={jsonMap(colours, "OTH")[1]} />
+                    <Area dataKey="lnpMost" type="stepAfter" activeDot={false} isAnimationActive={false} fill={jsonMap(colours, "LNP")[2]} />
+                    <Area dataKey="lnpMin" type="stepAfter" activeDot={false} isAnimationActive={false} fill={jsonMap(colours, "LNP")[1]} />
+                    <Area dataKey="lnpMaj" type="stepAfter" activeDot={false} isAnimationActive={false} fill={jsonMap(colours, "LNP")[0]} />
+                    <Tooltip />
+                </ComposedChart>
+            </ResponsiveContainer>
+        }
+        </>
+    )
+}
 
 const MainExplainer = props => {
     return (
@@ -91,7 +172,7 @@ const History = props => {
                     }
                     {historyValid &&
                         <StandardErrorBoundary>
-                            <LoadingMarker text="Done loading, this is a placeholder ..."/>
+                            <Chart data={history} />
                         </StandardErrorBoundary>
                     }
                     {!historyValid &&
