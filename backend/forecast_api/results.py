ced978856386adb240eeafd916b05bf54d3cfacc
d-j-hirst <djhir3@gmail.com>
2022-06-19 00:10:49 +1000

    Collect electoral results page URLs

--- backend/forecast_api/results.py
+++ backend/forecast_api/results.py
@@ -2,6 +2,7 @@ from forecast_api.models import Election
 import requests
 from bs4 import BeautifulSoup
 
+
 party_convert = {
     '2022fed': {
         'Labor': 'ALP',
@@ -24,6 +25,7 @@ party_convert = {
         'Independents': 'IND',
     },
 }
+
 
 class OverallResults:
     def __init__(self):
@@ -86,10 +88,51 @@ def fetch_overall_results(election: Election):
     overall_results.fp['OTH'] = 100 - total_fp
     return overall_results
 
+
+def collect_seat_names(election: Election):
+    # Use the "candidates" wikipedia page to get all the wikipedia links
+    # to all the electorates, it's the most straightforward way
+    election_wiki_desc_dict = {
+        'fed': '_Australian_federal_election',
+        'nsw': '_New_South_Wales_state_election',
+        'vic': '_Victorian_state_election',
+        'qld': '_Queenland_state_election',
+        'wa': '_Western_Australian_state_election',
+        'sa': '_South_Australian_state_election'
+    }
+    year = election.code[:4]
+    region = election.code[4:]
+    url = ('https://en.wikipedia.org/wiki/Candidates_of_the_' +
+        f'{year}{election_wiki_desc_dict[region]}')
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    tables = soup.find(class_='mw-parser-output').find_all(class_='wikitable')
+    urls = []
+    for table in tables:
+        rows = table.find_all('tr')
+        first_heading = rows[0].find('th')
+        if first_heading is None: continue
+        if first_heading.text.strip() != 'Electorate': continue
+        for row in rows:
+            first_cell = row.find('td')
+            if first_cell is None: continue
+            if '(' in first_cell.text: continue
+            link = first_cell.find('a')
+            if link is None: continue
+            seat_specific = link["href"].strip()[6:]
+            urls.append(f'https://en.wikipedia.org/wiki/Electoral_results_for_the_{seat_specific}')
+    return urls
+
+
 def update_results(election: Election):
     overall_results = fetch_overall_results(election)
+    urls = collect_seat_names(election)
+    collect_seat_names(election)
     for code, vote_share in list(overall_results.fp.items()):
         print(f'Overall {code} FP: {vote_share:.2f}%')
     for code, seats in list(overall_results.seats.items()):
         print(f'Overall {code} seats: {seats}')
     print(f'Overall ALP TPP: {overall_results.tpp:.2f}%')
+    print('Seat URLs')
+    for url in urls:
+        print(url)
