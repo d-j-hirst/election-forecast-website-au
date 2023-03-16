aebe45c7ad6c06399e1568d5dc98abd06e701561
d-j-hirst <djhir3@gmail.com>
2023-03-17 00:39:30 +1100

    Balmain no longer needs a warning

--- frontend/src/utils/seatwarnings.js
+++ frontend/src/utils/seatwarnings.js
@@ -1,5 +1,3 @@
 export const useWarning = (election, seatName) => {
-  return (
-    election === '2023nsw' && (seatName === 'Kiama' || seatName === 'Balmain')
-  );
+  return election === '2023nsw' && seatName === 'Kiama';
 };
