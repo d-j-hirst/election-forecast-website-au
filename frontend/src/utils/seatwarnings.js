2ebf2cdf1601c0d378e59dd606d8d047dce3ee4f
d-j-hirst <djhir3@gmail.com>
2025-08-01 16:20:53 +1000

    Add more seat warnings

--- frontend/src/utils/seatwarnings.js
+++ frontend/src/utils/seatwarnings.js
@@ -2,6 +2,13 @@ export const useWarning = (election, seatName) => {
   return (
     (election === '2023nsw' &&
       (seatName === 'Kiama' || seatName === 'Port Macquarie')) ||
-    (election === '2024qld' && seatName === 'Mirani')
+    (election === '2024qld' && seatName === 'Mirani') ||
+    (election === '2026sa' &&
+      (seatName === 'Mount Gambier' ||
+        seatName === 'Narungga' ||
+        seatName === 'MacKillop')) ||
+    (election === '2026vic' &&
+      (seatName === 'Prahran' || seatName === 'Narracan')) ||
+    (election === '2028fed' && seatName === 'Calare')
   );
 };
