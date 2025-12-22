f9794187733d5c71953ef61d50f6b985f19ad695
d-j-hirst <djhir3@gmail.com>
2025-12-23 07:48:01 +1100

    tweak seat warnings (Calare in 2025, not 2028)

--- frontend/src/utils/seatwarnings.js
+++ frontend/src/utils/seatwarnings.js
@@ -3,11 +3,11 @@ export const useWarning = (election, seatName) => {
     (election === '2023nsw' &&
       (seatName === 'Kiama' || seatName === 'Port Macquarie')) ||
     (election === '2024qld' && seatName === 'Mirani') ||
+    (election === '2025fed' && seatName === 'Calare') ||
     (election === '2026sa' &&
       (seatName === 'Narungga' || seatName === 'MacKillop')) ||
     (election === '2026vic' &&
       (seatName === 'Prahran' || seatName === 'Narracan')) ||
-    (election === '2027nsw' && seatName === 'Kiama') ||
-    (election === '2028fed' && seatName === 'Calare')
+    (election === '2027nsw' && seatName === 'Kiama')
   );
 };
