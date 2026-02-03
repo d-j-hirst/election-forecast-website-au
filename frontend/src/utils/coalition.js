81938490191aea315d2ac2eaa996da613198d570
d-j-hirst <djhir3@gmail.com>
2026-02-03 14:10:02 +1100

    leave out Nationals from "coalition" entirely in SA state

--- frontend/src/utils/coalition.js
+++ frontend/src/utils/coalition.js
@@ -2,8 +2,10 @@ export const coalitionName = termCode => {
   if (!termCode) return 'Liberal–National Coalition';
   const state = termCode.slice(4);
   const year = parseInt(termCode.slice(0, 4));
-  if (state === 'wa' || state === 'sa') {
+  if (state === 'wa') {
     return 'Liberal and National Parties';
+  } else if (state === 'sa') {
+    return 'Liberal Party';
   } else if (state === 'qld' && year >= 2008) {
     return 'Liberal National Party';
   } else if (state === 'fed' && year >= 2028) {
@@ -16,8 +18,10 @@ export const coalitionAbbreviation = termCode => {
   if (!termCode) return 'L–NC';
   const state = termCode.slice(4);
   const year = parseInt(termCode.slice(0, 4));
-  if (state === 'wa' || state === 'sa') {
+  if (state === 'wa') {
     return 'L/NP';
+  } else if (state === 'sa') {
+    return 'LIB';
   } else if (state === 'qld' && year >= 2008) {
     return 'LNP';
   } else if (state === 'fed' && year >= 2028) {
